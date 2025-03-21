7e9d47aacdfb4757affd35ec5fe329769b22710b
elevensheep <fttc100@gmail.com>
2025-03-21 19:01:59 +0900

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,11 @@ function Header() {
             <h1><Link to="/">책 추천 사이트</Link></h1> {/* 🔥 '/'로 이동하도록 변경 */}
             <nav className="nav">
                 <ul className="nav-menu">
-                    <li className="nav-item"><Link to="/category/경제">경제</Link></li>
-                    <li className="nav-item"><Link to="/category/정치">정치</Link></li>
-                    <li className="nav-item"><Link to="/category/스포츠">스포츠</Link></li>
-                    <li className="nav-item"><Link to="/category/사회">사회</Link></li>
-                    <li className="nav-item"><Link to="/category/국제">국제</Link></li>
+                    <li className="nav-item"><Link to="/category/politics">정치</Link></li>
+                    <li className="nav-item"><Link to="/category/economic">경제</Link></li>
+                    <li className="nav-item"><Link to="/category/sports">스포츠</Link></li>
+                    <li className="nav-item"><Link to="/category/society">사회</Link></li>
+                    <li className="nav-item"><Link to="/category/world">국제</Link></li>
                 </ul>
                 <input type="text" className="search-bar" placeholder="검색어를 입력하세요" />
             </nav>
